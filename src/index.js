da2db7e0bd740ba47aa0050b5683b4b19075bdb0
DamThiLanAnh <damthilananh8@hmail.com>
2024-12-22 18:21:01 +0700

    dap di xay lai

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.css'
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from 'react'
+import ReactDom from 'react-dom/client'
+
+function App() {
+  return (
+    <h1>Hello</h1>
+  )
+}
+
+const root = ReactDom.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
-
-reportWebVitals();
+)
