057df3a584a94fefc75065a7244961d324324727
DamThiLanAnh <damthilananh8@hmail.com>
2024-12-25 01:59:34 +0700

    product hook not complete

--- src/component/navbar.js
+++ src/component/navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-white border-bottom box-shadow py-3 px-5 mb-3">
@@ -11,10 +11,12 @@ const Navbar = () => {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="/">Home</a>
+                            <Link to="/"
+                                className="nav-link text-dark" aria-current="page" >Home</Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="/product">Product</a>
+                            <Link to="/product"
+                                className="nav-link text-dark">Product</Link>
                         </li>
                     </ul>
                     <form className="d-flex" role="search">
--- src/component/product.js
+++ src/component/product.js
@@ -1,9 +1,47 @@
-import React from 'react'
+import { React, useState } from 'react'
+function Product() {
+    const [content, setContent] = useState(<ProductList />)
 
-const Product = () => {
+    // Được gọi để render lại ProductList (danh sách sản phẩm).
+    // Gọi setContent để cập nhật nội dung state content thành <ProductList />.
+    // Truyền thêm prop showFrom={showForm} để liên kết nút "Create" với chức năng chuyển sang form tạo sản phẩm.
+    function showList() {
+        setContent(<ProductList showFrom={showForm} />)
+    }
+
+    // Được gọi để hiển thị ProductForm (form tạo sản phẩm).
+    // Gọi setContent để cập nhật nội dung state content thành <ProductForm />.
+    // Truyền thêm prop showList={showList} để liên kết nút "Cancel" với chức năng quay lại danh sách sản phẩm.
+    function showForm() {
+        setContent(<ProductForm showList={showList} />)
+    }
     return (
-        <div>product</div>
+        <div className='container my-5'>
+            {content}
+        </div>
+    )
+}
+export default Product
+
+function ProductList() {
+    return (
+        <>
+            <h2 className='text-center mb-3'>
+                List of products
+            </h2>
+            <button className='btn btn-primary me-2'>Create</button>
+        </>
     )
 }
 
-export default Product
+
+function ProductForm() {
+    return (
+        <>
+            <h2 className='text-center mb-3'>
+                Create of products
+            </h2>
+            <button className='btn btn-secondary me-2'>Cancel</button>
+        </>
+    )
+}
